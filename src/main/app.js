48bd2968b05145a25a12b6cda6b7534d0b57dda0
don <drowe2@lion.lmu.edu>
2012-04-14 20:57:16 -0700

    Server now starts up and responds at / and /reg. Runs on port 4000.

--- src/main/app.js
+++ src/main/app.js
@@ -3,3 +3,40 @@
  *
  * This is the main script for the FesTiPhus nameserver to run under node.
  */
+ 
+/*
+ * EXPRESS SERVER CONFIGURATION
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+//Hello world:
+app.get('/', function(req, res)
+{
+  res.send('SUP BRO');
+});
+
+//Register the address and port:
+app.get('/reg', function(req, res)
+{
+  /*for (var i = 0; i < req.length; i++)
+  {
+    console.log(req[i]);
+  }*/
+  //console.log(req);
+  var ipAddress = req.client.remoteAddress;
+  var port = req.client.remotePort;
+  res.send("<p>I got your address</p>" +
+           "<p>Your I.P. address is: " + ipAddress + "</p>" +
+           "<p>The port you used is: " + port + "</p>");
+});
+
+/*
+ * START THE SERVER
+ */
+
+app.listen(4000);
+console.log('Express server listening on port %d in %s mode', 
+    app.address().port, app.settings.env);
